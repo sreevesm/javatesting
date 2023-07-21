b2aa834bdb72f7ecde95a3bfd6dec2159c653d63
sreevesm <sreevesmathilde@hotmail.co.uk>
2023-07-21 14:23:14 +0100

    initial commit

---
+++ src/bootcamp/testing/pack/JavaTesting.java
@@ -0,0 +1,25 @@
+package bootcamp.testing.pack;
+
+public class JavaTesting {
+
+    public static void main(String[] args) {
+        String firstName = "mathilde";
+        String lastName = "sreeves";
+
+        System.out.println(firstName + " " + lastName);
+
+        int num1 = 2;
+        int num2 = 3;
+
+        System.out.println(num1 + num2);
+
+        int result = num1 / num2;
+        System.out.println(result);
+
+        int num3 = 8;
+        System.out.println(num3++);
+        System.out.println(num3);
+        System.out.println(++num3);
+    }
+}
+
